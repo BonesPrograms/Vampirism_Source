9589adfb6bdd76f92fa1202cfade74a3295d784c
BonesPrograms <devinbrown0322@gmail.com>
2026-02-06 21:52:45 -0500

    Updated VampirismStartGame to set DeathHandler.Player to null (same as we do on game load).

--- Scripts/Core/VampirismStartGame.cs
+++ Scripts/Core/VampirismStartGame.cs
@@ -3,15 +3,16 @@ using XRL.World;
 using XRL.UI;
 using Nexus.Rules;
 using Nexus.Core;
-using Nexus.Update;
+using XRL.World.Parts;
 
 [PlayerMutator]
 public class VampirismStartGame : IPlayerMutator
 {
     public void mutate(GameObject GO)
     {
+        DeathHandler.Player = null; //see more about why we do this in Core
         if (GO.IsVampire())
-        {
+        {  
             if (!Options.GetOptionBool(OPTIONS.HUNTER) && Options.GetOptionBool(OPTIONS.BLOODPACK))
                 GO.Inventory.AddObject("BloodCanteen");
             if (Options.GetOptionBool(OPTIONS.NIGHTBEAST))
